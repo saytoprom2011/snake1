80dda0d7b4559e17c59eccd3481c3de43096041d
saytoprom2011 <saytoprom@gmail.com>
2017-12-19 15:52:47 +0300

    VerticalLine and HorizontalLine

--- snake1/snake1/Program.cs
+++ snake1/snake1/Program.cs
@@ -9,65 +9,17 @@ namespace snake1
     {
         static void Main(string[] args)
         {
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
 
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-            numList.Add(3);
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Drow();
 
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<char> myNameList = new List<char>();
-            myNameList.Add('A');
-            myNameList.Add('B');
-            myNameList.Add('C');
-            myNameList.Add('D');
-
-            foreach (char i in myNameList)
-            {
-                Console.WriteLine(i);
-            }
-
-            List<char> charList = new List<char>();
-            charList.Add('@');
-            charList.Add('#');
-            charList.Add('$');
-            charList.Add('%');
-            charList.Add('&');
-            charList.Add('*');
-
-            foreach (char c in charList)
-            {
-                Console.WriteLine(c);
-            }
-
-
-            Point p1 = new Point(3, 3, '*');
-            Point p2 = new Point(4, 4, '#');
-            Point p3 = new Point(5, 5, '&');
-            Point p4 = new Point(6, 6, '%');
-            Point p5 = new Point(7, 7, '$');
-            Point p6 = new Point(8, 8, '@');
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-            pList.Add(p5);
-            pList.Add(p6);
-
-            foreach (Point p in pList)
-            {
-                p.Draw();
-            }
+            VerticalLine lineV = new VerticalLine(3, 12, 13, '%');
+            lineV.DrowV();
 
             Console.ReadLine();
         }
