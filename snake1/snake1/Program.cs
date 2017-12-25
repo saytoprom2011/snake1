fd5dbff3b96a88d3108cdd9e71105d4635dcbd6f
saytoprom2011 <saytoprom@gmail.com>
2017-12-25 14:08:16 +0300

    Figure and Ramki

--- snake1/snake1/Program.cs
+++ snake1/snake1/Program.cs
@@ -9,17 +9,28 @@ namespace snake1
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            // Отрисовка рамки
 
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
+
+            /*HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
             line.Drow();
 
             VerticalLine lineV = new VerticalLine(3, 12, 13, '%');
-            lineV.DrowV();
+            lineV.DrowV();*/
+
+            // Отрисовка точек
+            Point p = new Point(4, 5, '*');
+            p.Draw();
 
             Console.ReadLine();
         }
