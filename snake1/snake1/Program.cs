d410ff7e248677a9d7d2b998f6c5028f2f78b7d7
saytoprom2011 <saytoprom@gmail.com>
2018-01-09 18:42:10 +0300

    Snake can eat

--- snake1/snake1/Program.cs
+++ snake1/snake1/Program.cs
@@ -34,38 +34,33 @@ namespace snake
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
 
+
+            // Класс еды
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+
             while (true)
             {
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+                Thread.Sleep(100);
+
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
-                Thread.Sleep(200);
-                snake.Move();
             }
-
-            // Простое движение змейки
-            /*snake.Move();
-
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();*/
-
-            Console.ReadLine();
         }
-
     }
 }
  
